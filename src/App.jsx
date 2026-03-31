61bda6798e1600dd07bf3ace9c1862557d56a043
KacimiLahcen <kacimi.lahcen88@gmail.com>
2026-03-31 16:07:31 +0100

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
@@ -12,7 +12,8 @@ function App() {
   const plates = [
     { id: 1, name: "Pizza Margherita", price: 65, description: "Classic tomato and mozzarella", is_available: true },
     { id: 2, name: "Pasta Carbonara", price: 80, description: "Creamy pasta with bacon", is_available: false },
-    { id: 3, name: "Couscous Royal", price: 120, description: "Traditional Moroccan couscous", is_available: true }
+    { id: 3, name: "Couscous Royal", price: 120, description: "Traditional Moroccan couscous", is_available: true },
+    { id: 4, name: "Tanjiya kech", price: 75, description: "Traditional Marrrakech Tanjiya", is_available: true }
   ];
 
 
@@ -22,137 +23,72 @@ function App() {
 
   // const [count, setCount] = useState(0)
 
+
+
+  const [search, setSearch] = useState(""); //empty to contain search texxt;
+
+
+  const filteredPlates = plates.filter(plate =>
+    plate.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+
+
+
   return (
     <>
 
 
-    <div className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold text-center mb-8 text-orange-600">
-        Menu de Recommandation
-      </h1>
-      
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        
-        {/* send data by Props  */}
-        {plates.map(plate => (
-          <PlateCard 
-            key={plate.id} 
-            name={plate.name} 
-            price={plate.price} 
-            description={plate.description}
-            is_available={plate.is_available} 
-          />
-        ))}
+      <div className="min-h-screen bg-gray-100 p-8">
+        <h1 className="text-3xl font-bold text-center mb-8 text-orange-600">
+          Menu de Recommandation
+        </h1>
+
+        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6"> */}
+
+          {/* send data by Props  */}
+          {/* {plates.map(plate => (
+            <PlateCard
+              key={plate.id}
+              name={plate.name}
+              price={plate.price}
+              description={plate.description}
+              is_available={plate.is_available}
+            />
+          ))}
+        </div> */}
       </div>
-    </div>
 
 
 
-      {/* <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <div className="p-8">
+
+
+        <input
+          type="text"
+          placeholder="Find a plat"
+          className="border p-2 mb-4 w-full "
+          value={search}
+        onChange={(e) => setSearch(e.target.value)} // update state for each inserted text
+      />
+
+        <div className="grid gap-4">
+          {/* show filteredPlates or the msg if nothing found */}
+          {filteredPlates.length > 0 ? (
+            filteredPlates.map(plate => (
+              <PlateCard key={plate.id} {...plate} />
+            ))
+          ) : (
+            <p>Aucun plat trouvé.</p>
+          )}
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      </div>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section> */}
 
-      {/* <div className="ticks"></div>
-      <section id="spacer"></section> */}
+
+
+
     </>
   )
 }
